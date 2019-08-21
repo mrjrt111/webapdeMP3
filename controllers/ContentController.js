8379ae7cb53e502345885fc81c14ba8b81ea84da
Jarrett Ethan Singian <jarsingian@gmail.com>
2019-08-22 00:02:28 +0800

    fixed getOne

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -3,23 +3,35 @@ const router = express.Router()
 const bodyparser = require("body-parser")
 const auth = require("../middlewares/auth")
 const fs = require('fs')
-
+const multer = require("multer")
 const User = require("../models/User")
 const Content = require("../models/Content")
+
+
+//const UPLOAD_PATH = path.resolve(__dirname+ "../uploads")
+const upload = multer({
+    dest: 'uploads/',
+    limits: {
+        fileSize : 10000000,
+        files : 1
+    }
+})
 
 const urlencoder = bodyparser.urlencoded({
     extended : true
 })
 router.use(urlencoder)
 
-router.post("/createnotes", urlencoder, (req, res)=>{
+router.post("/createnotes",upload.single("note_image"), (req, res)=>{
+   // console.log("REQ:", req);
+
     let title = req.body.note_title;
     let note = req.body.note_content;
     let username = req.session.username;
     let checklistStrings = req.body.listitem;
+    let image = req.body.note_image;
 
-    console.log("BODY: ", req.body);
-    console.log("Title: ", title, " Note: ", note);
+
     var checklistJSON = [];
     for(var i in checklistStrings) {
 
@@ -30,15 +42,17 @@ router.post("/createnotes", urlencoder, (req, res)=>{
             "status"  : false
         });
     }
-
-
     console.log("ChecklistSchema Array: ", checklistJSON);
 
-        var noteContent = {title: title, username: username, note: note, checklist: checklistJSON};
+        var noteContent = {title: title, username: username, note: note, checklist: checklistJSON, image};
         Content.createContent(noteContent).then(()=>{
                 res.redirect("/");
         });
 
+
+})
+
+router.post("/editnotes", urlencoder, (req, res)=>{
 
 })
 module.exports = router;
